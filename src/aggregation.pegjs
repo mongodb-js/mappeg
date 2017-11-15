ba280445c5c396e9cb4d2e46c2440ddb20fb1137
Durran Jordan <durran@gmail.com>
2017-11-15 20:54:04 +0100

    Adding first tests for empty stages

---
+++ src/aggregation.pegjs
@@ -0,0 +1,11 @@
+stage
+  = expression
+
+expression
+ = begin_object end_object
+
+begin_object
+  = "{"
+
+end_object
+  = "}"
---
+++ test/parser.test.js
@@ -0,0 +1,35 @@
+const { expect } = require('chai');
+const parser = require('../');
+
+describe('parser', () => {
+  describe('#parse', () => {
+    context('when the stage is empty', () => {
+      context('when both open/close brackets exist', () => {
+        it('parses the empty stage', () => {
+          expect(parser.parse('{}')).to.deep.equal(['{', '}']);
+        });
+      });
+
+      context('when only an open bracket exists', () => {
+        it('raises a syntax error', () => {
+          expect(() => { parser.parse('{') }).
+            to.throw('Expected "}" but end of input found.');
+        });
+      });
+
+      context('when only a closing bracket exists', () => {
+        it('raises a syntax error', () => {
+          expect(() => { parser.parse('}') }).
+            to.throw('Expected "{" but "}" found.');
+        });
+      });
+
+      context('when no open or closing bracket exists', () => {
+        it('raises a syntax error', () => {
+          expect(() => { parser.parse('') }).
+            to.throw('Expected "{" but end of input found.');
+        });
+      });
+    });
+  });
+});
